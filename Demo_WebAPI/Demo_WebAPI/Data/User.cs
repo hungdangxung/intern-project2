82d7e986c4188511bfb24eba14e8fd1a4b637da9
hungdangxung <120209106+hungdangxung@users.noreply.github.com>
2023-02-21 17:13:40 +0700

    Add Register, Login

---
+++ Demo_WebAPI/Demo_WebAPI/Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo_WebAPI.Data
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Demo_WebAPI/Demo_WebAPI/Program.cs
+++ Demo_WebAPI/Demo_WebAPI/Program.cs
@@ -1,8 +1,35 @@
+using Demo_WebAPI.Data;
+using Demo_WebAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB"));
+});
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+var secretKey = builder.Configuration["AppSettings:SecretKey"];
+var secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
